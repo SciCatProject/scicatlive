e05a0c58e669f71782c453da25f9d0dc733a20cc
consolethinks <32138106+consolethinks@users.noreply.github.com>
2024-04-11 14:07:32 +0200

    Add sh,json javascript linters
    * add json and shell linting
    * add shell linting
    * add js linting

--- services/frontend/config/init.sh
+++ services/frontend/config/init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# shellcheck disable=SC2086
+# shellcheck disable=SC2010
 
 apk update && apk add jq
 
-[[ $BE_VERSION = "v4" ]] && CONFIGS=`find /config -name *.json ! -name "config.v3.json" | sort` || CONFIGS=`ls /config/*.json`
+[ $BE_VERSION = "v4" ] && CONFIGS=$(ls /config/*.json | grep -v 'config.v3.json') || CONFIGS=$(ls /config/*.json)
 
 jq -s 'reduce .[] as $item ({}; . * $item)' $CONFIGS > /usr/share/nginx/html/assets/config.json

3e3eb09cae3be34fa3e4520f215a914e234cd5a3
minottic <carlo.minotti1@gmail.com>
2024-04-12 10:41:31 +0200

    Simplify init.sh

--- services/frontend/config/init.sh
+++ services/frontend/config/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# shellcheck disable=SC2086
 # shellcheck disable=SC2010
 
 apk update && apk add jq
 
-[ $BE_VERSION = "v4" ] && CONFIGS=$(ls /config/*.json | grep -v 'config.v3.json') || CONFIGS=$(ls /config/*.json)
+[ "$BE_VERSION" = "v4" ] && EXCLUDE="[!v3]"
 
-jq -s 'reduce .[] as $item ({}; . * $item)' $CONFIGS > /usr/share/nginx/html/assets/config.json
+# shellcheck disable=SC2086
+jq -s 'reduce .[] as $item ({}; . * $item)' /config/*$EXCLUDE.json > /usr/share/nginx/html/assets/config.json

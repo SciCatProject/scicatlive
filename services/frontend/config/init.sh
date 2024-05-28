6c5e824fc1b71e5180dfc2f4dc04fc1edcfc0ed3
fpotier <frederic.potier@protonmail.com>
2024-05-28 14:58:38 +0200

    BE v3 config

--- services/frontend/config/init.sh
+++ services/frontend/config/init.sh
@@ -3,7 +3,9 @@
 
 apk update && apk add jq
 
-[ "$BE_VERSION" = "v4" ] && EXCLUDE="[!v3]"
+INCLUDE="base"
+[ "$BE_VERSION" = "v3" ] && INCLUDE="${INCLUDE} v3"
+[ -n "$LDAP_ENABLED" ] && INCLUDE="${INCLUDE} ldap"
 
-# shellcheck disable=SC2086
-jq -s 'reduce .[] as $item ({}; . * $item)' /config/*$EXCLUDE.json > /usr/share/nginx/html/assets/config.json
+# shellcheck disable=SC2046
+jq -s 'reduce .[] as $item ({}; . * $item)' $(for c in $INCLUDE; do echo "/config/config.${c}.json"; done) > /usr/share/nginx/html/assets/config.json

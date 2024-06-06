75187c618066f6b9926dc90d421a5666c87f268a
minottic <carlo.minotti1@gmail.com>
2024-06-06 13:30:56 +0200

    Use npm_config_yes instead of flag

--- services/backend/services/v3/config/init.sh
+++ services/backend/services/v3/config/init.sh
@@ -4,4 +4,4 @@
 FILES=$(ls /config/providers*.json)
 
 # shellcheck disable=SC2086
-npx --yes node-jq -s 'reduce .[] as $item ({}; . * $item)' $FILES > /home/node/app/server/providers.json
+npm_config_yes=true npx node-jq -s 'reduce .[] as $item ({}; . * $item)' $FILES > /home/node/app/server/providers.json

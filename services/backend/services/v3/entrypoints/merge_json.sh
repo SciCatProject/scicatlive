d7213a1f677ae998eafb394f8582df069c3b0bf9
minottic <carlo.minotti1@gmail.com>
2024-06-12 18:07:53 +0200

    Try to fix linting problems

--- services/backend/services/v3/entrypoints/merge_json.sh
+++ services/backend/services/v3/entrypoints/merge_json.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# shellcheck disable=SC2010
 
-for config in $(ls /config | xargs -n1 basename | cut -d "." -f 1 | sort -u)
+for config in $(find /config -maxdepth 1 -type f -exec basename {} \; | cut -d '.' -f 1 | sort -u)
 do
+    # shellcheck disable=SC2016
     npx --yes node-jq -s 'reduce .[] as $item ({}; . * $item)' /config/"${config}"*.json > /home/node/app/server/"${config}".json
 done

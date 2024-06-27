91d0eb19f0bb751d3777ed22769a09d71218714b
minottic <carlo.minotti1@gmail.com>
2024-06-27 10:35:23 +0200

    Simplify merge fe script

--- services/frontend/entrypoints/merge_json.sh
+++ services/frontend/entrypoints/merge_json.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
-# shellcheck disable=SC2010
 
 apk update && apk add jq
 
-FILES=$(ls /config/*.json)
-
-exclude_config () {
-    FILES=$(echo "$FILES" | xargs -n1 | grep -v "$1")
-}
-
-[ "$BE_VERSION" = "v4" ] && exclude_config "v3.json"
-[ -z "$LDAP_ENABLED" ] && exclude_config "ldap.json"
-[ -z "$OIDC_ENABLED" ] && exclude_config "oidc.json"
-
-# shellcheck disable=SC2086
-jq -s 'reduce .[] as $item ({}; . * $item)' $FILES > "${CONFIG_DIR:-/usr/share/nginx/html/assets}"/config.json
+jq -s 'reduce .[] as $item ({}; . * $item)' /config/*.json > "${CONFIG_DIR:-/usr/share/nginx/html/assets}"/config.json

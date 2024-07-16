1dd502afc56d76b706cddf9cfa428eeb319d98f3
minottic <carlo.minotti1@gmail.com>
2024-07-16 11:59:33 +0200

    Bind node-jq version to latest working

--- services/backend/services/v3/entrypoints/merge_json.sh
+++ services/backend/services/v3/entrypoints/merge_json.sh
@@ -3,5 +3,5 @@
 for config in $(find /config -maxdepth 1 -type f -exec basename {} \; | cut -d '.' -f 1 | sort -u)
 do
     # shellcheck disable=SC2016
-    npx --yes node-jq -s 'reduce .[] as $item ({}; . * $item)' /config/"${config}"*.json > /home/node/app/server/"${config}".json
+    npx --yes node-jq@6.0.0 -s 'reduce .[] as $item ({}; . * $item)' /config/"${config}"*.json > /home/node/app/server/"${config}".json
 done

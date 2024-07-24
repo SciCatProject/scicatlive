7147e8c8375fd890a49a18542408e7824783373f
Carlo Minotti <50220438+minottic@users.noreply.github.com>
2024-07-24 11:38:29 +0200

    Add job to check .mds links
    * Add job to check .mds links
    
    * Fix broken link 1
    
    * Fix broken link 2
    
    * Fix broken link 3
    
    * Fix broken link 4
    
    * Fix broken link 5
    
    * Fix broken link 6

--- .github/mkdocs/relative_to.py
+++ .github/mkdocs/relative_to.py
@@ -1,20 +1,31 @@
 from os import environ, path as ospath
-from urllib.parse import urljoin, urlsplit
+from urllib.parse import urljoin, urlsplit, urlunsplit
 from bs4 import BeautifulSoup
 from pathlib import Path
+from requests import get
 
 def on_page_content(html, page, config, files):
+    repo_url = f"{config.repo_url}/blob/{environ.get('TAG', 'main')}/"
+    doc_file = 'README.md'
+    check_links = environ.get('DOCS_LINK_CHECK')
     soup = BeautifulSoup(html, 'lxml')
     docs = files.documentation_pages()
     page_path = Path(page.url)
     for element in soup.find_all(href=True):
-        scheme, netloc, path, _, _ = urlsplit(element['href'])
-        if scheme or netloc: # External link
-            continue
-        if not path: # Self-link containing only query or anchor.
-            continue
+        base_url = repo_url
+        replace_href = False
+        scheme, netloc, path, query, fragment = urlsplit(element['href'])
         resolved_path = Path(ospath.normpath(page_path / path))
-        if any(filter(lambda x: Path(x.src_uri.strip('README.md')) == resolved_path, docs)): # Exiting docs
-            continue
-        element['href'] = urljoin(f"{config.repo_url}/blob/{environ.get('TAG', 'main')}/", ospath.normpath(page_path / element['href']))
+        if scheme or netloc: # External link
+            base_url = ''
+        elif not path: # Self-link containing only query or anchor.
+            resolved_path = resolved_path / doc_file
+        elif any(filter(lambda x: Path(x.src_uri.strip(doc_file)) == resolved_path, docs)): # Exiting docs
+            resolved_path = resolved_path / doc_file
+        else:
+            replace_href = True
+        relative_path = urlunsplit((scheme, netloc, str(resolved_path), query, fragment))
+        url = urljoin(base_url, relative_path)
+        if check_links: get(url).raise_for_status()
+        if replace_href: element['href'] = url
     return soup.prettify()
--- services/mongodb/README.md
+++ services/mongodb/README.md
@@ -14,7 +14,7 @@ These files are ingested into the database using mongo funcionalities and bypass
 
 ## Default configuration
 
-In the default configuration [init.js](./config/init.js), the seeding creates data in the mongodb database used by the `backend` service (either [v4](../backend/v4/), by default, or [v3](../backend/v3/) if specified otherwise by setting `BE_VERSION`).
+In the default configuration [init.js](./config/init.js), the seeding creates data in the mongodb database used by the `backend` service (either [v4](../backend/services/v4/), by default, or [v3](../backend/services/v3/) if specified otherwise by setting `BE_VERSION`).
 
 For an explanation of how setting `BE_VERSION` changes the environment creation see [here](../../README.md#docker-compose-profiles-and-env-variables-configuration-options).
 

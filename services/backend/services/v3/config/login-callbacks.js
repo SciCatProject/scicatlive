f5a92da1687d0ab5363a2fc23f7ff0b0bcd7e87b
minottic <carlo.minotti1@gmail.com>
2024-08-20 15:53:28 +0200

    Lint login callback

--- services/backend/services/v3/config/login-callbacks.js
+++ services/backend/services/v3/config/login-callbacks.js
@@ -3,11 +3,11 @@
 exports.accessGroupsToProfile =
   function (req, done) {
     return async function (err, user, identity, token) {
-      await user.identities.destroyAll({and: [
-        {provider: identity.provider},
-        {id: {neq: identity.id}},
-        {userId: user.id}
-      ]});
+      await user.identities.destroyAll({ and: [
+        { provider: identity.provider },
+        { id: { neq: identity.id } },
+        { userId: user.id }
+      ] });
       identity.updateAttributes({
         "profile": {
           accessGroups: identity.profile._json.accessGroups,

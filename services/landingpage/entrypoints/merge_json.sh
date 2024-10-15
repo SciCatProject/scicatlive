dc327d53894d1b1117d0a8351bde0e319c413294
Carlo Minotti <50220438+minottic@users.noreply.github.com>
2024-10-15 14:48:52 +0200

    Move fe and landingpage entrypoints to common (#326)
    * Move fe and landingpage entrypoints to common
    
    * Rename tests to karma_tests as it is now shared

--- services/landingpage/entrypoints/merge_json.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apk update && apk add jq gettext
-
-jq -s 'reduce .[] as $item ({}; . * $item)' /config/*.json | envsubst \
-    > "${CONFIG_DIR:-/usr/share/nginx/html/assets}"/config.json

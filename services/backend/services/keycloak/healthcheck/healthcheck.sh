1f084078eba025c4618e427df323dcb21265c0b2
Spencer Bliven <spencer.bliven@gmail.com>
2024-11-21 11:47:22 +0100

    Fix healthcheck for keycloak 26
    The healthcheck was failing for two reasons:
    1. --health-enabled now defaults to false 
    2. It validates HTTP requests more strictly. Thus a bug in the GET
       request from healthcheck.sh was successful in 25.0 but gave a 400
       response in 26.0

--- services/backend/services/keycloak/healthcheck/healthcheck.sh
+++ services/backend/services/keycloak/healthcheck/healthcheck.sh
@@ -4,7 +4,7 @@ if ! exec 3<>/dev/tcp/localhost/9000; then
     exit 1
 fi
 
-printf "GET /health/ready HTTP/1.1\r\nHost: localhostConnection: close\r\n\r\n" >&3
+printf "GET /health/ready HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n" >&3
 timeout --preserve-status 5 grep -m 1 "UP" <&3
 exit_status=$?
 exec 3>&-

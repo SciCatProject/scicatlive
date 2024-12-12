db02ea9c03d591b88dace4aeb3473390b11e1e6b
minottic <carlo.minotti1@gmail.com>
2024-12-12 15:36:14 +0100

    Add flock semaphor to prevent multiple unison runs

--- entrypoints/bidirectional_backup.sh
+++ entrypoints/bidirectional_backup.sh
@@ -4,6 +4,7 @@ apk add --no-cache unison
 
 [ -z "$(ls -A /bbackup)"] && cp -r "${WORKDIR}"/. /bbackup
 
-unison "${WORKDIR}" /bbackup \
+flock -n /bbackup/.unison \
+    unison "${WORKDIR}" /bbackup \
     -repeat watch -batch -auto \
     -prefer /bbackup &

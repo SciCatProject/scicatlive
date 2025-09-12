cf8b0a96bd4fb10b4a620b19b74e88b9d0e80493
minottic <carlo.minotti1@gmail.com>
2025-09-12 16:07:38 +0200

    Fix brakets and restore datasource

--- services/backend/services/v3/entrypoints/tests.sh
+++ services/backend/services/v3/entrypoints/tests.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-echo '{{"mongo": {"database": "dacat_dev"}}}' > /home/node/app/server/datasources.json
+cp /home/node/app/server/datasources.json /home/node/app/server/datasources.json.bak
+
+echo '{"mongo": {"database": "dacat_dev"}}' > /home/node/app/server/datasources.json
 
 npm run test
+
+mv /home/node/app/server/datasources.json.bak /home/node/app/server/datasources.json

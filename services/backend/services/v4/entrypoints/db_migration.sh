026a3571fc98a9bb032c168633e6bae1dc27e924
fpotier <frederic.potier@protonmail.com>
2025-10-17 14:21:34 +0200

    fix: database migration
    `npm run db:migrate:up` now needs modules from the main app to run
    - `npm run build` is ran in DEV mode prior to migrating the db
    - in the prod image, everything is already built so the migration script
      is ran directly (without using npm)

--- services/backend/services/v4/entrypoints/db_migration.sh
+++ services/backend/services/v4/entrypoints/db_migration.sh
@@ -2,6 +2,11 @@
 
 [ -e ".finished" ] && return 0
 
+if [ -e "src" ]; then
+    # in DEV mode, a build is needed before running the migration scripts
+    npm run build
+fi
+
 echo "MONGODB_URI=$MONGODB_URI" >.env
-npm run migrate:db:up
+node ./node_modules/migrate-mongo/bin/migrate-mongo.js up
 rm .env

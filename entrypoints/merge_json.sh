c1202853afb51471319ad48c3f7023de26d0b445
minottic <carlo.minotti1@gmail.com>
2025-10-24 10:59:15 +0200

    chore: use one single config.override file

--- entrypoints/merge_json.sh
+++ entrypoints/merge_json.sh
@@ -5,4 +5,4 @@
 apk update && apk add jq gettext
 
 jq -s 'reduce .[] as $item ({}; . * $item)' /config/*.json | envsubst \
-    > "${CONFIG_DIR:-/usr/share/nginx/html/assets}"/config.json
+    > "${CONFIG_DIR:-/usr/share/nginx/html/assets}"/"${CONFIG_FILE:-config.json}"

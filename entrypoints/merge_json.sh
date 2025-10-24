31dd41773f0c3dac9eb6d07c76b252e6b52dbbd9
minottic <carlo.minotti1@gmail.com>
2025-10-24 14:34:47 +0200

    Fallback to apt if apk is missing
    This is needed by the nginx container that uses apt

--- entrypoints/merge_json.sh
+++ entrypoints/merge_json.sh
@@ -2,7 +2,8 @@
 
 [ -e ".finished" ] && return 0
 
-apk update && apk add jq gettext
+apk update && apk add jq gettext || echo "apk not available"
+apt update && apt install -y jq gettext-base || echo "apt not available"
 
 jq -s 'reduce .[] as $item ({}; . * $item)' /config/*.json | envsubst \
     > "${CONFIG_DIR:-/usr/share/nginx/html/assets}"/"${CONFIG_FILE:-config.json}"

ca3fc90d3b6c00af3f26c39788580d98f1c7ff7c
André Baumberger <github.legume822@silomails.com>
2026-08-25 14:33:50 +0200

    fix: - linting errors

--- services/ingestor/entrypoints/download_data.sh
+++ services/ingestor/entrypoints/download_data.sh
@@ -7,9 +7,11 @@ COLLECTION_NAME="single_particle_test"
 if ! [ -d $COLLECTION_ROOT/$COLLECTION_NAME ]; then
     if command -v apk >/dev/null 2>&1; then 
 	    apk update && apk add ca-certificates git 
-    fi
-    if command -v apt >/dev/null 2>&1; then 
+    elif command -v apt >/dev/null 2>&1; then 
         apt update && apt install -y ca-certificates git
+    else 
+         echo "No supported package manager found (apk/apt)" >&2
+        exit 1
     fi
     
     update-ca-certificates
@@ -17,11 +19,13 @@ if ! [ -d $COLLECTION_ROOT/$COLLECTION_NAME ]; then
     echo "Downloading tutorial data"
 
     git clone --depth 1 --filter=blob:none --sparse https://github.com/osc-em/oscem-extractor-life.git
+
+    (
+        cd oscem-extractor-life ||  exit 1
+        git sparse-checkout set tutorial
+        mkdir -p $COLLECTION_ROOT
+        mv tutorial $COLLECTION_ROOT/$COLLECTION_NAME
+    )
     
-    cd oscem-extractor-life
-    git sparse-checkout set tutorial
-    mkdir -p $COLLECTION_ROOT
-    mv tutorial $COLLECTION_ROOT/$COLLECTION_NAME
-    cd ..
     rm -rf oscem-extractor-life
 fi

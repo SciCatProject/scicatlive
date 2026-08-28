2cf38c4241710c8cf07074520ce2304e3eb108dc
Carlo Minotti <50220438+minottic@users.noreply.github.com>
2026-08-28 09:48:56 +0200

    chore: limit to v4 and avoid depends (#926)

--- services/backend/services/v3/entrypoints/ensure_docs_folder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-## The v3 backend repo has no docs/ folder of its own, but the docs service mounts one from this volume regardless -
-## create it so that mount doesn't fail.
-mkdir -p docs
